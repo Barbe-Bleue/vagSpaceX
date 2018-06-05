5992a798af09f61f4a301ee08728e657474d7a8a
Guigui <screy@icloud.com>
2018-06-05 15:16:01 +0200

    #2 rocket data api (#9)
    * #2 call api for rocket
    
    * #2 add rocket provider

--- src/app/modules/app-routing/app-routing.module.ts
+++ src/app/modules/app-routing/app-routing.module.ts
@@ -1,20 +1,26 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { RouterModule, Routes } from "@angular/router";
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {RouterModule, Routes} from "@angular/router";
 
-import { AppComponent } from "../../app.component";
-import { CompanyInfoComponent } from "../../components/company-info/company-info.component";
+import {AppComponent} from "../../app.component";
+import {CompanyInfoComponent} from "../../components/company-info/company-info.component";
+import {RocketListComponent} from "../../components/rocket-list/rocket-list.component";
 
 const appRoutes: Routes = [
-  {
-    path: "company-info",
-    component: CompanyInfoComponent
-  }
+    {
+        path: "company-info",
+        component: CompanyInfoComponent
+    },
+    {
+        path: "rockets",
+        component: RocketListComponent
+    }
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forRoot(appRoutes)],
-  exports: [RouterModule],
-  declarations: []
+    imports: [CommonModule, RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule],
+    declarations: []
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
