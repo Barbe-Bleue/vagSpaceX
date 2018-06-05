dbdf0710272933d7b9218da96e03126711ce20c9
Valentin LENFANT <valentin.lenfant@hotmail.com>
2018-06-05 16:04:50 +0200

    #12 Fix le bordel je parle en franglais lol (#13)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,27 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { MatInputModule, MatFormFieldModule } from '@angular/material';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { FormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
-import { AppComponent } from "./app.component";
-import { AppRoutingModule } from "./modules/app-routing/app-routing.module";
-import { SharedModule } from "./modules/shared/shared.module";
-import { CompanyInfoComponent } from "./components/company-info/company-info.component";
-
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {MatInputModule, MatFormFieldModule} from "@angular/material";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {AppComponent} from "./app.component";
+import {AppRoutingModule} from "./modules/app-routing/app-routing.module";
+import {SharedModule} from "./modules/shared/shared.module";
+import {CompanyInfoComponent} from "./components/company-info/company-info.component";
+import {RocketListComponent} from "./components/rocket-list/rocket-list.component";
+import { HugeNumberPipe } from "./pipes/hugeNumber.pipe";
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CompanyInfoComponent,
-  ],
-  imports: [BrowserModule,HttpClientModule, AppRoutingModule, SharedModule, MatFormFieldModule , MatInputModule, FormsModule, MatCheckboxModule],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CompanyInfoComponent,
+        RocketListComponent,
+        HugeNumberPipe
+    ],
+    imports: [BrowserModule, HttpClientModule, AppRoutingModule, SharedModule, MatFormFieldModule, MatInputModule, FormsModule, MatCheckboxModule],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
