517f36fe0252f1a4e93d870349b906001b1a6a0e
Barbe-Bleue <screy@icloud.com>
2018-06-07 20:57:54 +0200

    add style

--- src/app/components/launches-home/launches-home.component.ts
+++ src/app/components/launches-home/launches-home.component.ts
@@ -18,11 +18,13 @@ export class LaunchesHomeComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.isLoading = true;
     const today = new Date();
     this.launchService.fetchLatestLaunch().subscribe((data: Launch) => {
       this.launchService.fetchUpcomingLaunches().subscribe((dataUpcoming: Launch[]) => {
         this.upcomingLaunches = dataUpcoming;
         this.nextLaunches = this.upcomingLaunches[0].launch_date_utc.replace("T", " ").replace("Z", "").replace(".000", "");
+        this.isLoading = false;
       }
     });
   });
